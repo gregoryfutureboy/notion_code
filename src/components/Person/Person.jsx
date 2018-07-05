af02101ac4250a1932bbfb18c7ff3c7b2f42e68a
Futureboy <gregory@human.space>
2018-07-05 01:12:20 -0400

    list & states

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Person = () => {
+const Person = (props) => {
   return (
-    <div>
-      Hello Person Component
+    <div onClick={props.onclick}>
+      <h1>{props.name}</h1>
+      <h3>{props.age}</h3>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -6,12 +6,64 @@ import Person from './components/Person/Person';
 class App extends Component {
 
   state = {
+    persons: [
+      { id: 1, name: 'Gregory', age: 24 },
+      { id: 2, name: 'Philippe', age: 24 },
+      { id: 3, name: 'Sacha', age: 25 }
+    ],
+    showPersons: false
+  }
+
+  handleToggleList = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    })
+  }
+
+  handleDeletePerson = (index) => {
+    const personsToDelete = [...this.state.persons];
+    personsToDelete.splice(index, 1)
+    this.setState({
+      persons: personsToDelete
+    })
+  }
+
+  handleRefreshList = () => {
+    this.setState({
+      persons: [
+        { id: 1, name: 'Gregory', age: 24 },
+        { id: 2, name: 'Philippe', age: 24 },
+        { id: 3, name: 'Sacha', age: 25 }
+      ]
+    })
   }
 
   render() {
+
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        this.state.persons.map((person, index) => {
+          return <Person
+                    name={person.name}
+                    age={person.age}
+                    onclick={() => this.handleDeletePerson(index)}
+                    key={person.id}
+                  />
+        })
+      )
+    }
+
     return (
       <div>
-        <Person />
+        <button
+          onClick={this.handleToggleList}
+        >Toggle List</button>
+        <button
+          onClick={this.handleRefreshList}
+        >Refresh List</button>
+        {persons}
       </div>
     )
   }
