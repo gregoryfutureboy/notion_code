dd3131bf2cf3a523d41c93e26dbe98b10f28cb6c
Futureboy <gregory@human.space>
2018-07-04 23:55:17 -0400

    rendering conditional components 002

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Person from './components/Person/Person';
+
 class App extends Component {
 
   state = {
-    showHello: false,
+    showPersons: null,
   }
 
-  handleToggleHello = () => {
+  handleToggleComponent = () => {
     this.setState({
-      showPersons: !this.state.showPersons
+      showPersons: !this.state.showPersons,
     })
   }
 
   render() {
+    //Write the conditional expression within the render method because
+    //React re-renders everything that is within this method. Hence, once
+    //the conditional is met, it will re-render and you'll see the proper
+    //JSX appear.
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <Person />
+      )
+    }
+
     return (
       <div>
         <button
-          onClick={this.handleToggleHello}>Toggle Hello</button>
-        {this.state.showPersons ? <h1>Hello</h1> : null}
+          onClick={this.handleToggleComponent}
+        >Toggle Component</button>
+        {persons}
       </div>
     )
   }
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
